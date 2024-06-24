e7fcf35447930fb765c15f4e9a0e8d3c052d3cf2
Camille Haus <camille_haus@hotmail.com>
2024-06-24 20:10:50 -0300

    Cleaning up code

--- src/middleware/isCategoryOwner.middleware.ts
+++ src/middleware/isCategoryOwner.middleware.ts
@@ -1,15 +1,10 @@
 import { NextFunction, Response, Request } from "express";
-import { prisma } from "../database/prisma";
 import { AppError } from "../errors/appError";
 
 
 export class IsCategoryOwner {
     static async execute (req: Request, res: Response, next: NextFunction) {
          const userId = res.locals.decode.id;
-
-         
-
-        
 
          if(res.locals.category.user.id !== userId) {
             throw new AppError(403, "This user is not the category owner")
