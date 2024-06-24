32e22f75be12cdf15e2b028c5199b32bc26716b0
Camille Haus <camille_haus@hotmail.com>
2024-06-24 20:18:11 -0300

    Deploy settings

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import "express-async-errors";
+import cors from "cors";
 import express, { json } from "express";
 import helmet from "helmet";
 import { categoryRouter } from "./routes/categories.routes";
@@ -14,6 +15,8 @@ const PORT = 3000;
 app.use(helmet())
 
 app.use(json());
+
+app.use(cors())
 
 app.use("/tasks", tasksRouter);
 app.use("/categories", categoryRouter);
